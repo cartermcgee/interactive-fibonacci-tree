78f6d4c3166dae32846adf233ae74afa406acafc
Carter McGee <cartermcgee7@gmail.com>
2019-03-18 19:29:29 -0600

    list slider is working, need to start working on the tree slider

--- src/fib.js
+++ src/fib.js
@@ -1,32 +1,66 @@
 // Re-set the document title
-document.title = 'TODO: Dynamic Fibonacci Sequence in JavaScript';
+document.title = 'Dynamic Fibonacci Sequence';
 
-// Create a red div in the body of the document
-var div = document.createElement('div');
-div.setAttribute('class', 'red fib-container');
-document.querySelector('body').appendChild(div);
+//create a div to hold the fibonacci sequence as a list
+var listDiv = document.createElement('div');
+listDiv.setAttribute('class', 'red fib-container');
+document.querySelector('body').appendChild(listDiv);
 
-// Make a paragraph to hold some instructions
-var para = document.createElement('p');
-para.textContent = "Write code necessary to";
-div.appendChild(para);
+//create the form which contains the slider
+var listForm = document.createElement('form');
+listForm.setAttribute('id', 'list-of-divs');
 
-var ol = document.createElement('ol');
-div.appendChild(ol)
+var label = document.createElement('label');
+label.setAttribute('id', 'list-label');
+label.setAttribute('for', 'list-slider');
+label.textContent = "List(0)";
 
-var li = document.createElement('li');
-li.textContent = "create a <form>";
-ol.appendChild(li);
+var input = document.createElement('input');
+input.setAttribute('id', 'list-slider');
+input.setAttribute('type', 'range');
+input.setAttribute('min', '0');
+input.setAttribute('max', '20');
+input.setAttribute('value', '0');
+input.setAttribute('oninput', 'listSlider(this)');
 
-li = document.createElement('li');
-li.textContent = "an <input> of type 'range' with the 'onchange' attribute which calls a JavaScript function";
-ol.appendChild(li);
-
-li = document.createElement('li');
-li.textContent = "as well as any other functions needed to compute the Fibonacci numbers recursively";
-ol.appendChild(li);
+listForm.appendChild(label);
+listForm.appendChild(input);
+listDiv.appendChild(listForm);
 
 
-li = document.createElement('li');
-li.textContent = "and create a <div> for each recursive function call";
-ol.appendChild(li);
+//function that turns a number into its fibonacci sequence value
+var numToFib = function(num) {
+    if (num==1) return 1;
+    if (num==0) return 0;
+    return numToFib(num - 1) + numToFib(num - 2);
+}
+
+//function that takes input from the list slider and displays the fib. sequence to that value
+var listSlider = function(me) {
+    
+    //change the label to match the current value
+    var value = me.value
+    var changeLabel = document.querySelector('#list-label');
+    changeLabel.textContent = `List(${value})`;
+    
+    //remove all previous values from the div and initialize our empty div
+    var fibList = document.querySelector('div.fib-list');
+    if (fibList) fibList.remove();
+    fibList = document.createElement('div');
+    fibList.setAttribute('class', 'fib-list');
+    
+    //add divs containing a fib number to the fib-list div
+    for (var i = 0; i <= value; i++) {
+        var d = document.createElement('div');
+        d.setAttribute('class', 'fib-item');
+        var p = document.createElement('p');
+        p.textContent = numToFib(i);
+        d.appendChild(p);
+        fibList.appendChild(d);
+    }
+    
+    //add to document
+    var theForm = document.querySelector('#list-of-divs');
+    theForm.appendChild(fibList);
+}
+
